b385f795d1e43fdfb61a472d5b77f8c56ea2af06
Warlin Reyes <warlin16@gmail.com>
2018-01-02 15:23:16 -0500

    Added thunk actions for sessions and a sessions reducer along with session errors reducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,36 @@
+import * as SessionApiUtil from '../util/session_api_util';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+
+export const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  };
+};
+
+export const receiveErrors = (errors) => {
+  return {
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+  };
+};
+
+export const login = user => dispatch {
+  return SessionApiUtil.login(user).then(user =>
+    dispatch(receiveCurrentUser(user)),
+    err => dispatch(receiveErrors(err.responseJSON)));
+};
+
+export const signup = user => dispatch => {
+  return SessionApiUtil.signup(user).then(user =>
+   dispatch(receiveCurrentUser(user)),
+   err => dispatch(receiveErrors(err.responseJSON)));
+};
+
+export const logout = () => dispatch => {
+  return SessionApiUtil.logout().then(() => {
+    return dispatch(receiveCurrentUser(null));
+  });
+};
