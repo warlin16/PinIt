1cb3693579eef49a89607de057ae6ba144e15765
Warlin Reyes <warlin16@gmail.com>
2018-01-02 17:11:30 -0500

    Deploying app

--- frontend/pin_it.jsx
+++ frontend/pin_it.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-
+import Root from './components/root';
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
   const store = configureStore();
   window.getState = store.getState;
-  ReactDOM.render(<h1>Hey... is this thing on?</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
