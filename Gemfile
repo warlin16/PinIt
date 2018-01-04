fe2066d5f517f46b488fe19cc339d44531553223
Warlin Reyes <warlin16@gmail.com>
2018-01-03 22:08:02 -0500

    Added a few Css animation and bits

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'jquery-rails'
+gem 'pry-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -40,7 +41,6 @@ group :development, :test do
   gem 'selenium-webdriver'
   gem 'better_errors'
   gem 'binding_of_caller'
-  gem 'pry-rails'
   gem 'annotate'
 end
 
--- frontend/components/sessions/session_form.jsx
+++ frontend/components/sessions/session_form.jsx
@@ -4,6 +4,7 @@ import { Link, withRouter } from 'react-router-dom';
 class SessionForm extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       username: '',
       password: ''
@@ -16,9 +17,6 @@ class SessionForm extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.loggedIn) {
-      this.props.history.push('/');
-    }
   }
 
   update(field) {
@@ -43,50 +41,54 @@ class SessionForm extends React.Component {
 
   renderErrors() {
     return(
-      <ul className={`${this.props.formType}-errors`}>
-        {this.props.errors.map((error, i) => (
-          <li key={`error-${i}`}>
-            {error}
-          </li>
-        ))}
-      </ul>
+      <div className={`${this.props.formType}-errors animated fadeIn`}>
+        <ul>
+          {this.props.errors.map((error, i) => (
+            <li key={`error-${i}`}
+              className='animated fadeInLeft'>
+              {error}
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 
   render() {
     const _class = this.props.formType;
     return (
-      <div className={`${_class}-form-container`}>
-        {this.navLink()}
-        <div className={`${_class}-logo`}>
-          <img src={window.staticImages.pinitLogo} />
-        </div>
-        <div className={`${_class}-greeting`}>
-          <h1>{this.props.formGreeting}</h1>
-        </div>
-        <form onSubmit={this.handleSubmit} className={`${_class}-form-box`}>
-          <div className={`${_class}-form`}>
-            <label className={`${_class}-user`}>
-              <input type="text"
-                placeholder="Create Username"
-                value={this.state.username}
-                onChange={this.update('username')}
-                className={`${_class}-input`}
-                onClick={() => this.setState({ username: ''})}
-              />
-            </label>
-            <label className={`${_class}-password`}>
-              <input type="password"
-                placeholder="Create Password"
-                value={this.state.password}
-                onChange={this.update('password')}
-                className={`${_class}-input`}
-              />
-            </label>
-            <input type="submit" value={`${this.props.submitButton}`} className={`${_class}-submit`} />
+      <div className={`${_class}-background`}>
+        <div className={`${_class}-form-container`}>
+          {this.navLink()}
+          <div className={`${_class}-logo`}>
+            <img src={window.staticImages.pinitLogo} />
           </div>
-        </form>
-        {this.renderErrors()}
+          <div className={`${_class}-greeting`}>
+            <h1>{this.props.formGreeting}</h1>
+          </div>
+          <form onSubmit={this.handleSubmit} className={`${_class}-form-box`}>
+            <div className={`${_class}-form`}>
+              <label className={`${_class}-user`}>
+                <input type="text"
+                  placeholder="Create Username"
+                  value={this.state.username}
+                  onChange={this.update('username')}
+                  className={`${_class}-input`}
+                />
+              </label>
+              <label className={`${_class}-password`}>
+                <input type="password"
+                  placeholder="Create Password"
+                  value={this.state.password}
+                  onChange={this.update('password')}
+                  className={`${_class}-input`}
+                />
+              </label>
+              <input type="submit" value={`${this.props.submitButton}`} className={`${_class}-submit`} />
+            </div>
+          </form>
+          {this.renderErrors()}
+        </div>
       </div>
     );
   }
