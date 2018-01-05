d13e99c73418a0717e410789213e2506c923bcff
Warlin Reyes <warlin16@gmail.com>
2018-01-05 10:52:25 -0500

    Added the layout for the whole user show page

--- frontend/components/users/user_show.jsx
+++ frontend/components/users/user_show.jsx
@@ -7,17 +7,34 @@ class UserShow extends React.Component {
   }
 
   render() {
+    const user = this.props.user ? this.props.user : { username: '' };
     return(
       <div className='show-container'>
-        <div className='main-link-div'>
-          <Link to='/'> BACK TO THE FEED BRO</Link>
+        <div className='user-bio-box'>
+          <div className='user-bio'>
+            <div className='user-info'>
+              <div className='user-name'><h3>{user.username}</h3></div>
+            </div>
+            <div className='user-img'>
+              This is where an IMG will go
+            </div>
+          </div>
+
+
+          <div className='content-selector'>
+            <Link to='/'>FEED BRO</Link>
+            <button>BOARDS</button>
+            <button>PINS</button>
+          </div>
+
         </div>
-          <div className='main-info-box'>
-            <h1> MY
-              NAME IS {this.props.user ? this.props.user.username : ""}
-              AND I'M IN A DIV
+
+          <div className='user-content-box'>
+            <h1>
+              And they said I couldn't be a div... Fools!
             </h1>
           </div>
+
       </div>
     );
   }
