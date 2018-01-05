1d99a15aabbd650ee680d58ce68015382310d34b
Warlin Reyes <warlin16@gmail.com>
2018-01-05 14:13:34 -0500

    Added styling for board index

--- frontend/components/users/user_show.jsx
+++ frontend/components/users/user_show.jsx
@@ -14,26 +14,71 @@ class UserShow extends React.Component {
           <div className='user-bio'>
             <div className='user-info'>
               <div className='user-name'><h3>{user.username}</h3></div>
+
+              <div className='follows'>
+                <div className='followers'>
+                  <div>0</div>
+                    <div>followers</div>
+                  </div>
+                <div className='following'>
+                  <div>0</div>
+                    <div>following</div>
+                  </div>
+              </div>
             </div>
+
             <div className='user-img'>
-              This is where an IMG will go
+              <div><img src={window.staticImages.tempLogo} /></div>
             </div>
           </div>
 
-
           <div className='content-selector'>
-            <Link to='/'>FEED BRO</Link>
+            <Link to='/'>BACK TO FEED</Link>
             <button>BOARDS</button>
             <button>PINS</button>
           </div>
 
         </div>
 
-          <div className='user-content-box'>
-            <h1>
-              And they said I couldn't be a div... Fools!
-            </h1>
-          </div>
+          <section className='user-content-box'>
+            <div className='user-content'>
+              <div>
+                <h1>
+                  And they said I couldn't be a div... Fools!
+                </h1>
+              </div>
+
+              <div>
+                <h1>
+                  testing 😅
+                </h1>
+              </div>
+
+              <div>
+                <h1>
+                  IN A DIV 😎
+                </h1>
+              </div>
+
+              <div>
+                <h1>
+                  IN A DIV 😎
+                </h1>
+              </div>
+
+              <div>
+                <h1>
+                  IN A DIV 😎
+                </h1>
+              </div>
+
+              <div>
+                <h1>
+                  I should be on the second row lol
+                </h1>
+              </div>
+            </div>
+          </section>
 
       </div>
     );
