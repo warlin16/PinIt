7b992beb99f4e7289a3e90a371a9d8f7f7be6b4a
Warlin Reyes <warlin16@gmail.com>
2018-02-19 11:11:15 -0500

    remove debugger

---
+++ frontend/components/modals/pin_it_modal.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class PinItForm extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="pin-show-container">
+        <div className="pin-it-box">
+
+        </div>
+      </section>
+    );
+  }
+}
