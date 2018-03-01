9db3b37ae15324892a5718c13b93b2ae9fde729f
Warlin Reyes <warlin16@gmail.com>
2018-03-01 12:23:41 -0500

    start user profile img modal

--- frontend/components/modals/user_edit_modal.jsx
+++ frontend/components/modals/user_edit_modal.jsx
@@ -10,7 +10,26 @@ class UpdateUserForm extends Component {
       <section
         className="create-board-container"
         onClick={this.props.closeModal}
-      />
+      >
+        <div
+          className="board-form-container animated fadeIn"
+          onClick={this.props.stopPropagation}
+        >
+          <div className="pin-file-box">
+            <div>
+              <label>
+                Click to edit profile img!
+                <input type="file" onChange={this.updateFile} />
+              </label>
+
+              <img src={window.staticImages.camera} />
+            </div>
+          </div>
+          <div className="pin-form-submit">
+            <button>Submit</button>
+          </div>
+        </div>
+      </section>
     );
   }
 }
