6a9f95ae7baffd2218cee6679d2a0c701e9e487f
Warlin Reyes <warlin16@gmail.com>
2018-03-21 20:23:03 -0400

    add timestamps to pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180220222347) do
+ActiveRecord::Schema.define(version: 20180322001640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20180220222347) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.bigint "attachment_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["attachment_id"], name: "index_pins_on_attachment_id"
     t.index ["author_id"], name: "index_pins_on_author_id"
     t.index ["board_id"], name: "index_pins_on_board_id"
