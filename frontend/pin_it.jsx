19d413e5c2c5e0ad925a1e7e8a19147a77af1e49
Warlin Reyes <warlin16@gmail.com>
2022-04-02 19:52:45 -0400

    comment

--- frontend/pin_it.jsx
+++ frontend/pin_it.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
 
+
+// useless
+
 document.addEventListener("DOMContentLoaded", () => {
   let store;
   if (window.currentUser) {
